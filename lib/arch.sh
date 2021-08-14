adb2e726aebba01fa8a1086b13b31f5bf438dce0
F <f@dxw.com>
2021-08-14 21:20:47 +0200

    Confirm we're on macos

--- lib/arch.sh
+++ lib/arch.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+macos() {
+  [ "$(uname -s)" = "Darwin" ]
+}
+
 arm64() {
   [ "$(uname -m)" = "arm64" ]
 }
 
 m1() {
-  arm64 || [ "$(sysctl -in sysctl.proc_translated)" = "1" ]
+  macos && (arm64 || [ "$(sysctl -in sysctl.proc_translated)" = "1" ])
 }
