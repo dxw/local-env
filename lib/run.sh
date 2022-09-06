f6a7d3daf4a1eedf4e70063d516b9c065a9c575e
F <f@dxw.com>
2022-09-06 23:37:04 +0200

    Support retrying run commands if they fail

--- lib/run.sh
+++ lib/run.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
+source lib/print.sh
+
 run() {
   echo -e "\x1B[1;34m==\$ $*\x1B[0m"
   eval "$*"
+}
+
+run_with_retry() {
+  if ! run "$*"; then
+    say "It looks like the last step didn't finish completely."
+    say "You can either try running it again or continue as is."
+    say "It's unlikely to cause any major issues if you continue."
+
+    if ask_Yn "Retry the last step?"; then
+      run_with_retry "$*"
+    fi
+  fi
 }
